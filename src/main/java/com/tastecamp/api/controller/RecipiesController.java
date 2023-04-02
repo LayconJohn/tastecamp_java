9a1783bb39dcc3c8e2e3fe5e08d23a68a63e190b
layconjohn <layconjohn@gmail.com>
2023-04-02 00:12:51 -0300

    feat: validation and controller deteleById

--- src/main/java/com/tastecamp/api/controller/RecipiesController.java
+++ src/main/java/com/tastecamp/api/controller/RecipiesController.java
@@ -3,7 +3,9 @@ package com.tastecamp.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tastecamp.api.dto.RecipiesDTO;
 import com.tastecamp.api.models.Recipie;
 import com.tastecamp.api.repository.RecipieRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/person")
@@ -26,7 +30,12 @@ public class RecipiesController {
     }
     
     @PostMapping
-    public void create(@RequestBody RecipiesDTO req) {
+    public void create(@RequestBody @Valid RecipiesDTO req) {
         repository.save(new Recipie(req));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }
--- src/main/java/com/tastecamp/api/dto/RecipiesDTO.java
+++ src/main/java/com/tastecamp/api/dto/RecipiesDTO.java
@@ -2,11 +2,24 @@ package com.tastecamp.api.dto;
 
 import com.tastecamp.api.models.Level;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record RecipiesDTO(
-    String name, 
-    String ingredients, 
-    String preparation, 
-    int time, 
+
+    @NotBlank
+    String name,
+    
+    @NotBlank
+    String ingredients,
+    
+    @NotNull
+    String preparation,
+    
+    @NotNull
+    int time,
+    
+    @NotBlank
     Level level
     ) {
         
