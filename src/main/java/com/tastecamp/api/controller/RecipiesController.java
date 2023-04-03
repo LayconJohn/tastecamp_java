0fc77d7125938bbc708fff0a00e5471df04e9b78
layconjohn <layconjohn@gmail.com>
2023-04-03 00:53:48 -0300

    feat: get Recipie By Id and put recipie by id

--- src/main/java/com/tastecamp/api/controller/RecipiesController.java
+++ src/main/java/com/tastecamp/api/controller/RecipiesController.java
@@ -1,14 +1,17 @@
 package com.tastecamp.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tastecamp.api.dto.RecipiesDTO;
@@ -28,10 +31,27 @@ public class RecipiesController {
     public List<Recipie> getAll() {
         return repository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public Optional<Recipie> getById(@PathVariable Long id) {
+        return repository.findById(id);
+    }
     
     @PostMapping
     public void create(@RequestBody @Valid RecipiesDTO req) {
         repository.save(new Recipie(req));
+    }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable Long id, @RequestBody @Valid RecipiesDTO req) {
+        repository.findById(id).map(item -> {
+            item.setName(req.name());
+            item.setIngredients(req.ingredients());
+            item.setPreparation(req.preparation());
+            item.setTime(req.time());
+            item.setLevel(req.level());
+            return repository.save(item);
+        });
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/com/tastecamp/api/dto/RecipiesDTO.java
+++ src/main/java/com/tastecamp/api/dto/RecipiesDTO.java
@@ -4,10 +4,13 @@ import com.tastecamp.api.models.Level;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public record RecipiesDTO(
 
     @NotBlank
+    @Size(min = 3, max = 30)
     String name,
     
     @NotBlank
@@ -17,6 +20,7 @@ public record RecipiesDTO(
     String preparation,
     
     @NotNull
+    @Positive
     int time,
     
     @NotBlank
