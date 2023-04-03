19c1edb2eebb955bec0cd9c336cee563b6e6a37a
layconjohn <layconjohn@gmail.com>
2023-04-03 10:49:25 -0300

    refactor: create http status on controller

--- src/main/java/com/tastecamp/api/controller/RecipiesController.java
+++ src/main/java/com/tastecamp/api/controller/RecipiesController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tastecamp.api.dto.RecipiesDTO;
@@ -27,26 +28,30 @@ public class RecipiesController {
     @Autowired
     private RecipieService service; 
 
+    @ResponseStatus(value = HttpStatus.OK)
     @GetMapping
     public List<Recipie> getAll() {
         return service.getAll();
     }
-
+    @ResponseStatus(value = HttpStatus.OK)
     @GetMapping("/{id}")
     public Optional<Recipie> getById(@PathVariable Long id) {
         return service.getById(id);
     }
     
+    @ResponseStatus(value = HttpStatus.CREATED)
     @PostMapping
     public void create(@RequestBody @Valid RecipiesDTO req) {
         service.create(new Recipie(req));
     }
 
+    @ResponseStatus(value = HttpStatus.CREATED)
     @PutMapping("/{id}")
     public void update(@PathVariable Long id, @RequestBody @Valid RecipiesDTO req) {
         service.updateById(id, req);
     }
 
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         service.deleteById(id);
